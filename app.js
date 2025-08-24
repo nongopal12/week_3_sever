ed56f70bb5f0476547f5a75fb043c5c50fde138e
nongopal12 <panuphong2712@gmail.com>
2025-08-24 17:11:06 +0700

    1

--- app.js
+++ app.js
@@ -1,11 +1,130 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-
 const app = express();
+
+
 const con = require('./db');
 
-app.use(express.json());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//---------------------- generate password 
+// http://localhost:3000/password/1111
+app.get('/password/:raw',(req,res)=>{
+    const raw = req.params.raw;
+    bcrypt.hash(raw,10,(err,hash)=>{
+        if(err){
+            return res.status(500).send('Error creating password!');
+        }
+        res.send(hash);
+    });
+});
+
+
+// ----------------- login ------------------
+app.post('/login',(req,res)=>{
+    const {username, password} = req.body;
+    const sql = "SELECT * FROM users WHERE username = ?";
+    con.query(sql,[username],(err,result)=>{
+        if(err){
+            return res.status(500).send('Database error login!');
+        }
+        if(result.length != 1){
+            return res.status(401).send('Invalid username or password!');
+        }
+        bcrypt.compare(password,result[0].password,(err,same)=>{
+            if(err){
+                return res.status(500).send('Error comparing passwords!');
+            }
+            if(same){
+                res.status(200).json({
+                    message:"Login successful!",
+                    user_id: result[0].id,
+                });
+            }else{
+                res.status(401).send('Invalid username or password!');
+            }
+        })
+    })
+})
+
+
+// ----------------- register ------------------
+app.post('/register',(req,res)=>{
+    const {username, password} = req.body;
+        const sql = "SELECT * FROM users WHERE username = ?";
+        con.query(sql,[username],(err,result)=>{
+            if(err){
+                return res.status(500).send('Database error!');
+            }
+            if(result.length > 0){
+                return res.status(400).send('Username already exists!');
+            }
+            bcrypt.hash(password, 10,(err,hash)=>{
+                if(err){
+                    return res.status(500).send('Error creating password!');
+                }
+                const insert = "INSERT INTO users (username, password) VALUES (?, ?)";
+                con.query(insert,[username, hash],(err,result)=>{
+                    if(err){
+                        return res.status(500).send("Database error! Can't insert new account.");
+                    }
+                    res.status(200).send('User registered successfully!');
+                })
+            })
+
+
+        })
+   
+})
+
+
+
+
+// get all expenses
+app.get('/expenses/:user_id', (req, res) => {
+    const userId = req.params.user_id;
+    const sql = "SELECT * FROM expense WHERE user_id = ?";
+    con.query(sql,[userId],(err,result)=>{
+        if(err){
+            return res.status(500).send('Database error!');
+        }
+        res.json(result);
+    })
+})
+
+
+// get all today's expense
+app.get('/expenses/today/:user_id', (req, res) => {
+    const userId = req.params.user_id;
+   
+    const sql = "SELECT * FROM expense WHERE user_id = ? AND DATE(date) = CURDATE()";
+    con.query(sql,[userId],(err,result)=>{
+        if(err){
+            return res.status(500).json({error: 'Database error!'});
+        }
+        res.json(result);
+    })
+})
+
+// resarch expense
+    //เขียนตรงนี้
+
+
+
+// Add new expense
+    //เขียนตรงนี้
+
+
+
+// Delete an expense
+    //เขียนตรงนี้
+
+
+
+    
 app.listen(3000,()=>{
-    console.log('sever is running EZ')
+    console.log('Server is running on port 3000 ✅');
 })
